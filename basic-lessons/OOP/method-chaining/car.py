2753f854bddf2b08255dcc921263cb8a96dc0e3a
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-09-16 04:03:03 -0300

    Continuando POO, estudos sobre encadeamento de métodos

---
+++ basic-lessons/OOP/method-chaining/car.py
@@ -0,0 +1,18 @@
+
+class Car():
+
+    def turn_on(self):
+        print("You start the engine")
+        return self
+
+    def drive(self):
+        print("You drive the car")
+        return self
+
+    def brake(self):
+        print("You step on the break")
+        return self
+
+    def turn_off(self):
+        print("You turn off the engine")
+        return self
