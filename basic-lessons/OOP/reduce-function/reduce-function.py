fb0b6ccdecb6839ac825fedd890f587482ab4aaf
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-09-25 02:41:59 -0300

    Função reduce()

---
+++ basic-lessons/OOP/reduce-function/reduce-function.py
@@ -0,0 +1,13 @@
+# reduce() = apply a function to an iterable and reduce it to a single cumulative value
+#            performs function on first two elements and repeats process until 1 value remains
+# reduce(function, iterable)
+
+import functools
+
+# letters = ["H", "E", "L", "L", "O"]
+# word = functools.reduce(lambda x, y: x+y, letters) # x = first elements, y = second element (H, E) then (HE, L)... until HELLO
+# print(word)
+
+factorial = [5, 4, 3, 2, 1]
+result = functools.reduce(lambda x, y: x*y, factorial) # this time we multiply the elements
+print(result)
