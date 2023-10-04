f4f68479dd4b4fb8cc83fd9b2e4018bc6811b584
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-10-04 01:04:39 -0300

    Modificações na formatação do código

--- basic-lessons/OOP/lambda-function/lambda-function.py
+++ basic-lessons/OOP/lambda-function/lambda-function.py
@@ -8,18 +8,17 @@
 
 # print(double(5))
 
-double = lambda x:x * 2
+double = lambda x: x * 2
 print(double(5))
 
 multiply = lambda x, y: x * y
 print(multiply(5, 25))
 
 add = lambda x, y, z: x + y + z
-print(add(10,30,50))
+print(add(10, 30, 50))
 
-full_name = lambda first_name, last_name: first_name+" "+last_name
+full_name = lambda first_name, last_name: first_name + " " + last_name
 print(full_name("Rougert Brian", "Alexandre"))
 
 age_check = lambda age: True if age >= 18 else False
 print(age_check(17))
-
