f567be03a42c90217da5ae781d7a7efaf75c29e7
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-10-10 05:04:44 -0300

    Aprendendo criação e manipulão de telas (new window) e abas (window tabs)

---
+++ basic_lessons/GUI_windows_related/new_window_gui.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+def create_window():
+    # new_window = Toplevel() # Toplevel() = new window "on top" of other window, linked to a "bottom" window
+    new_window = Tk()  # Tk() = New independent window
+    old_window.destroy()  # close old window
+
+
+old_window = Tk()
+
+Button(old_window, text="New Window", command=create_window).pack()
+
+old_window.mainloop()
