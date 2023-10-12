e0b9ae6209accbf837e0b1538a81e9869375628d
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-10-12 10:17:00 -0300

    Bug fix

--- basic_lessons/GUI_windows_related/src/animations_gui.py
+++ basic_lessons/GUI_windows_related/src/animations_gui.py
@@ -2,14 +2,13 @@ from tkinter import *
 import time
 import random
 
-#  Kinda buggy but works... or sorta
-
 WIDTH = 1200
 HEIGHT = 800
 x_velocity = random.randint(1, 5)
 y_velocity = random.randint(1, 5)
 
 window = Tk()
+window.resizable(False, False)
 
 alien = PhotoImage(file="assets/alien.png")
 space = PhotoImage(file="assets/cartoon_space.png")
@@ -23,23 +22,24 @@ alien_image = canvas.create_image(0, 0, image=alien, anchor=NW)
 image_width = alien.width()
 image_height = alien.height()
 
+label_coordinates = Label(canvas, font=("DS-Digital", 15))
+label_coordinates.place(x=1000, y=50)
+label_speed = Label(canvas, font=("DS-Digital", 15))
+label_speed.place(x=950, y=80)
+
 while True:
     coordinates = canvas.coords(alien_image)
-    # print(coordinates)
-    if coordinates[0] > (WIDTH-(image_width-5)) or coordinates[0] < 0:
-        if x_velocity > 0:
-            x_velocity = -(random.randint(1, 5))
-            print(f"x = {x_velocity}")
-        elif x_velocity < 0:
-            x_velocity = random.randint(1, 5)
-            print(f"x = {x_velocity}")
-    if coordinates[1] > (HEIGHT-(image_height-5)) or coordinates[1] < 0:
-        if y_velocity > 0:
-            y_velocity = -(random.randint(1, 5))
-            print(f"y = {y_velocity}")
-        elif y_velocity < 0:
-            y_velocity = random.randint(1, 5)
-            print(f"y = {y_velocity}")
+    label_coordinates.config(text=f"X: {int(coordinates[0])} Y: {int(coordinates[1])}")
+    label_speed.config(text=f"Speed X: {x_velocity} Speed Y: {y_velocity}")
+
+    if coordinates[0] > (WIDTH-(image_width-5)):
+        x_velocity = -(random.randint(1, 5))
+    if coordinates[0] < 0:
+        x_velocity = random.randint(1, 5)
+    if coordinates[1] > (HEIGHT-(image_height-5)):
+        y_velocity = -(random.randint(1, 5))
+    if coordinates[1] < 0:
+        y_velocity = random.randint(1, 5)
     canvas.move(alien_image, x_velocity, y_velocity)
     window.update()
     time.sleep(0.01)
