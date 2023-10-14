16c7f5fc32b464d6cd8666ff6e4b7bf4ad715246
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-10-14 14:48:07 -0300

    rodando python via cmd e instalando e atualizando pip

---
+++ basic_lessons/cmd_run_py.py
@@ -0,0 +1,11 @@
+# run .py file with cmd
+
+# save file as .py, go to cmd, navigato do directory
+# cd: ... invoke python interpreter + script: python "name.py"
+
+
+print("Hello World")
+
+name = input("What's your name? ")
+
+print(f"Hello {name}")
---
+++ basic_lessons/pip_package_manager.py
@@ -0,0 +1,15 @@
+# Python pip
+
+# pip = package manager for packages and modules from Python Package Index
+
+# included for Python 3.4+
+
+# open cmd
+
+# help: pip
+# check: pip --version
+# update: pip install --upgrade pip
+# installed packages: pip list
+# check outdated packages: pip list --outdated
+# update a package: pip install "package_name" --upgrade
+# install a package: pip install "package name"
